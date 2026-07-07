631b876d2fc51a5a8d0f1317d19eabfcb0e72bad
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-07 23:46:13 +0530

    ✅ Top K Frequent Words | Medium | Java | Runtime: 0 ms | Memory: 43 MB | 2026-07-07

--- leetcode/medium/0692-top-k-frequent-words/solution.java
+++ leetcode/medium/0692-top-k-frequent-words/solution.java
@@ -10,24 +10,17 @@ class Solution {
             map.put(ch , freq + 1);
         }
         // System.out.print(map);
-        PriorityQueue<String> pq = new PriorityQueue<>(
-            (a, b) -> {
-                if (map.get(a).equals(map.get(b))) {
-                    return b.compareTo(a);   // reverse lexicographical
-                }
-                return map.get(a) - map.get(b); // smaller frequency first
-            }
-        );
+        PriorityQueue<String> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(String ch : map.keySet()){
            pq.add(ch);
            if(pq.size() > k){
-            pq.remove();
+            pq.remove(ch);
            }
         }
         while(!pq.isEmpty()){
             res.add(pq.remove());
         }
-        // Collections.reverse(res);
+        // Collections.reverse(res); 
         return res;
     }
 }
