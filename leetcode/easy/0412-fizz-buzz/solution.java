f3958612b19f9d968abfd8595ac9564e252ab414
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-10 23:00:59 +0530

    ✅ Fizz Buzz | Easy | Java | Runtime: 0 ms | Memory: 42.6 MB | 2026-07-10

---
+++ leetcode/easy/0412-fizz-buzz/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> res = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+               res.add("FizzBuzz");
+            }else if(i % 3 == 0){
+                res.add("Fizz");
+            }else if(i % 5 == 0){
+                res.add("Buzz");
+            }
+        }
+        return res;
+    }
+}
