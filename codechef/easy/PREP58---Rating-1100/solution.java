4ba54280e16caa529a1af11e3a699d6545813372
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-20 14:21:25 +0530

    ✅ PREP58 - Rating 1100 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-20

---
+++ codechef/easy/PREP58---Rating-1100/solution.java
@@ -0,0 +1,28 @@
+/* Node is defined as
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
+*/
+
+
+class Solution {
+    public static Node detectCycle(Node head){
+        // Add code here
+        Node temp = head;
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return fast;
+            }
+        }
+        return null;
+    }
+}
