e7685197b1913c48dbb1c339cddbccf2d3b9f777
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-20 19:51:31 +0530

    ✅ ACT01 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-20

--- codechef/medium/ACT01/solution.java
+++ codechef/medium/ACT01/solution.java
@@ -4,18 +4,23 @@ import java.io.*;
 
 class Codechef
 {
-    public static int sub(int  n , int p){
-        int res = n - p;
-        return res;
+    public static int sub(int arr[]){
+        int ele = 0;
+        for(int i = 1; i < arr.length; i++){
+            ele = Math.abs(arr[i] - arr[i - 1]);
+        }
+        return ele;
     }
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int p = sc.nextInt();
-		
-		sub(n , p);
+		int arr[] = new int[n];
+		for(int i = 0; i < n; i++){
+		    arr[i] = sc.nextInt();
+		}
+		sub(arr);
 
 	}
 }
