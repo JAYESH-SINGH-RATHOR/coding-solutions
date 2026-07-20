e0f79be5d0cb7fd3e6ec1f11afe4ee3048d467fc
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-20 20:01:37 +0530

    ✅ CANDY01 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-20

--- codechef/medium/CANDY01/solution.java
+++ codechef/medium/CANDY01/solution.java
@@ -4,9 +4,27 @@ import java.io.*;
 
 class Codechef
 {
+    public static void candyy(int child[] , int candy){
+        int sum = 0;
+        for(int i = 0; i < child.length; i++){
+            sum += child[i];
+            if(sum <= candy){
+                System.out.print("Yes");
+            }
+            System.out.print("No");
+        }
+    }
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i = 0; i < n; i++){
+		    arr[i] = sc.nextInt();
+		}
+		int m = sc.nextInt();
+		candyy(arr, m);
 
 	}
 }
