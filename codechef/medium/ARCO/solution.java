1e84f2bb2dc3d07f53ea7335ffa5df89b36be7e9
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-27 19:59:25 +0530

    ✅ ARCO | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/medium/ARCO/solution.java
+++ codechef/medium/ARCO/solution.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int count(int arr[]){
+        
+    }
     public static void main(String[] args) {
         // Write your code here
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0 ; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        count(arr);
     }
 }
