dc8e61a39e948991a45dc03675de78e03ceec9c0
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-27 20:50:54 +0530

    ✅ ROCU | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/medium/ROCU/solution.java
+++ codechef/medium/ROCU/solution.java
@@ -7,19 +7,6 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n + 1];
-        for(int i = 1; i <= n; i++){
-            arr[i] = sc.nextInt();
-        }
-        long dp[] = new long[n + 1];
-        dp[0] = 0;
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
-                dp[i] = Math.max(dp[i] , arr[j] + dp[i - j]);
-            }
-        }
-        System.out.print(dp[n]);
+
 	}
 }
