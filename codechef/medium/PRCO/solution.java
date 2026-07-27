37b10637b41bb5d65a2ba94343550e861ccdd65f
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-27 20:21:08 +0530

    ✅ PRCO | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-27

---
+++ codechef/medium/PRCO/solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static int count(String s){
+        
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+
+	}
+}
