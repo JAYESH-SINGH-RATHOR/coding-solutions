e061c25d3ed13a6cc546fd8a38840b1b543da2e9
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-27 20:28:59 +0530

    ✅ PRCO | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/medium/PRCO/solution.java
+++ codechef/medium/PRCO/solution.java
@@ -5,11 +5,34 @@ import java.io.*;
 class Codechef
 {
     public static int count(String s){
-        
+        int left = 0; 
+        int len = s.length();
+        int count = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return makepalindrome(s , left + 1 , right) || return 
+                makepalindrome(s , left , right - 1);
+            }else{
+                count = Math.max(len , count);
+            }
+            System.out.print(count);
+            return count;
+        }
+        public static boolean makepalindrome(String s , int left , int right){
+            while(left < right){
+                if(s.charAt(left) == s.charAt(right)){
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+            count(s);
 	}
 }
