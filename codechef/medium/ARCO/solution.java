f526a08c5b444e15f65a4794ab12616b894ffd8a
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-27 20:23:54 +0530

    ✅ ARCO | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-27

--- codechef/medium/ARCO/solution.java
+++ codechef/medium/ARCO/solution.java
@@ -1,38 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int count(int arr[]) {
-        int length = arr.length;
-        for (int i = 1; i < length; i++) {
-            if (arr[i] == arr[i - 1]) {
-                length = remove(arr, i, length);
-                i--; 
-            }
-        }
-
-        System.out.println( length);
-               return length;
+    public static int count(int arr[]){
+        
     }
-
-    public static int remove(int arr[], int index, int length) {
-        for (int i = index; i < length - 1; i++) {
-            arr[i] = arr[i + 1];
-        }
-        return length - 1;
-    }
-
     public static void main(String[] args) {
-
+        // Write your code here
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-
         int arr[] = new int[n];
-
-        for (int i = 0; i < n; i++) {
+        for(int i = 0 ; i < n; i++){
             arr[i] = sc.nextInt();
         }
-
         count(arr);
     }
 }
