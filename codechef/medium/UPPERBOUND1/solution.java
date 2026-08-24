54dcaaf3d5b2a546b48bb041f6ad6d1d51a74977
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-24 06:29:38 +0530

    ✅ UPPERBOUND1 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/UPPERBOUND1/solution.java
+++ codechef/medium/UPPERBOUND1/solution.java
@@ -3,7 +3,7 @@
      int left = 0;
      int right = nums.length - 1;
      int idx = nums.length;
-     while(left < right){
+     while(left <= right){
          int mid = left + (right - left)/2;
          if(nums[mid] > x){
              idx = mid;
