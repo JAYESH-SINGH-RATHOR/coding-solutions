361740452619923b6eace26c142d3a6cb201e650
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-28 19:44:00 +0530

    ✅ Contains Duplicate | Easy | Java | Runtime: 61 ms (5.13%) | Memory: 110 MB | 2026-08-28

--- leetcode/easy/0217-contains-duplicate/solution.java
+++ leetcode/easy/0217-contains-duplicate/solution.java
@@ -1,12 +1,18 @@
-public class Solution {
+class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (!set.add(num)) {
-                return true; 
-            }
+       HashMap<Integer , Integer > map = new HashMap<>();
+       for(int e : nums){
+        if(!map.containsKey(e)){
+            map.put(e , 0);
         }
-        return false; 
-       
+        int freq = map.get(e);
+        map.put(e , freq + 1);
+       }
+       for(int e : map.keySet()){
+        if(map.get(e) > 1){
+            return true;
+        }
+       }
+       return false;
     }
 }
