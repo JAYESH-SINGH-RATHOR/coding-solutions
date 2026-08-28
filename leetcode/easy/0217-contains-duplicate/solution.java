475d241f3fbe7bfeef01a20d903063e9751e2ffe
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-28 19:45:14 +0530

    ✅ Contains Duplicate | Easy | Java | Runtime: 23 ms (31.80%) | Memory: 81.4 MB | 2026-08-28

--- leetcode/easy/0217-contains-duplicate/solution.java
+++ leetcode/easy/0217-contains-duplicate/solution.java
@@ -1,18 +1,31 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//        HashMap<Integer , Integer > map = new HashMap<>();
+//        for(int e : nums){
+//         if(!map.containsKey(e)){
+//             map.put(e , 0);
+//         }
+//         int freq = map.get(e);
+//         map.put(e , freq + 1);
+//        }
+//        for(int e : map.keySet()){
+//         if(map.get(e) > 1){
+//             return true;
+//         }
+//        }
+//        return false;
+//     }
+// }
+
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       HashMap<Integer , Integer > map = new HashMap<>();
-       for(int e : nums){
-        if(!map.containsKey(e)){
-            map.put(e , 0);
-        }
-        int freq = map.get(e);
-        map.put(e , freq + 1);
-       }
-       for(int e : map.keySet()){
-        if(map.get(e) > 1){
+      Arrays.sort(nums);
+      for(int i = 1; i < nums.length; i++){
+        if(nums[i] == nums[i - 1]){
             return true;
         }
-       }
-       return false;
+      }
+      return false;
     }
 }
