2ff5c71a46d49ef8879695b2f90c72d627e124c3
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-03 11:18:49 +0530

    ✅ Delete Nodes From Linked List Present in Array | Medium | Java | Runtime: 0 ms | Memory: 42.1 MB | 2026-09-03

--- leetcode/medium/3217-delete-nodes-from-linked-list-present-in-array/solution.java
+++ leetcode/medium/3217-delete-nodes-from-linked-list-present-in-array/solution.java
@@ -8,23 +8,53 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode modifiedList(int[] nums, ListNode head) {
+//       ListNode dummy = new ListNode(0);
+//       dummy.next = head;
+//       ListNode curr = dummy;
+//       Set<Integer> set = new HashSet<>();
+//       for(int e : nums){
+//         set.add(e);
+//       }
+//       while(curr.next != null){
+//         if(set.contains(curr.next.val)){
+//             curr.next = curr.next.next;
+//         }
+//        else{
+//          curr = curr.next;
+//        }
+//       }
+//       return dummy.next;
+//     }
+// }
+
+// optimized way // 
+
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-      ListNode dummy = new ListNode(0);
-      dummy.next = head;
-      ListNode curr = dummy;
-      Set<Integer> set = new HashSet<>();
-      for(int e : nums){
-        set.add(e);
-      }
-      while(curr.next != null){
-        if(set.contains(curr.next.val)){
-            curr.next = curr.next.next;
+        // Dummy node to handle head removals
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = dummy;
+
+        while (curr.next != null) {
+            boolean remove = false;
+            // Check if curr.next.val is in nums
+            for (int e : nums) {
+                if (curr.next.val == e) {
+                    remove = true;
+                    break;
+                }
+            }
+            if (remove) {
+                // Skip the node
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
         }
-       else{
-         curr = curr.next;
-       }
-      }
-      return dummy.next;
+
+        return dummy.next;
     }
 }
